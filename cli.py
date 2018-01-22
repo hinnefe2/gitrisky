2f0b9d3b6dab9c25b8e8452df68720953206758a
Henry Hinnefeld <henry.hinnefeld@gmail.com>
2018-01-21 20:03:36 -0600

    ENH Implement cli train, predict fns

--- cli.py
+++ cli.py
@@ -2,6 +2,12 @@
 
 import click
 
+from sklearn.ensemble import RandomForestClasifier
+
+from model import save_model, load_model
+from gitcmds import get_latest_commit
+from parsing import get_features, get_labels
+
 
 @click.group
 def cli():
@@ -10,11 +16,51 @@ def cli():
 
 @cli.command()
 def train():
-    """Train a git commit bug risk model"""
-    pass
+    """Train a git commit bug risk model.
+
+    This will save a pickled sklearn model to a file in the toplevel directory
+    for this repository.
+    """
+
+    # get the features and labels by parsing the git logs
+    features = get_features()
+    labels = get_labels()
+
+    # instantiate and train a model
+    model = RandomForestClasifier()
+    model.train(features, labels)
+
+    print('Trained a model on {n} training examples with {n_bug} positive cases'
+          .format(n=len(features), n_bug=sum(labels)))
+
+    # pickle the model to a file in the top level repo directory
+    save_model(model)
 
 
 @cli.command()
-def predict():
-    """Score a git commit bug risk model"""
-    pass
+@click.option('-c', '--commit', type=str)
+def predict(commit):
+    """Score a git commit bug risk model
+
+    Parameters
+    ----------
+    commit: str
+        The hash of the commit to score.
+
+    Raises
+    ------
+    NotFittedError
+        If a gitrisky model has not yet been trained on the currrent repo.
+    """
+
+    model = load_model()
+
+    if commit is None:
+        commit = get_latest_commit()
+
+    features = get_features(commit)
+
+    score = model.predict_proba(features)
+
+    print('Commit {commit} has a bug score of {score} / 1.0'
+          .format(commit=commit, score=score))
